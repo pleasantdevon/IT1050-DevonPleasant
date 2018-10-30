2faa0f100e54ac3b5b2151edab572e1b351b80e6
PLEASANT 22 <pleasantdevon22@gmail.com>
2018-10-29 23:23:56 -0400

    Created lab 01

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        //Main method begins execution of C# app
+        static void Main(string[] args)
+        {
+            NewMethod(); //end Main
+        }
+
+        private static void NewMethod() => Console.WriteLine("Hello World from Devon Pleasant");
+    } //end class Program
+}
