68c23877cde2399bd8a12737141dd723052f9ee3
pleasantdevon <44344101+pleasantdevon@users.noreply.github.com>
2018-11-12 18:39:48 -0500

    Add files via upload

---
+++ Lab03/Lab03Question7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            while (x <= 20)
+            {
+                Console.WriteLine(x);
+                x += 2;
+ 
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
