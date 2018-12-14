649b5485c64f997d2c62e12cc65d02ee59d02b87
pleasantdevon <44344101+pleasantdevon@users.noreply.github.com>
2018-12-14 09:32:41 -0500

    Add files via upload

---
+++ midterm7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midterm7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8;
+            int i = 10; // initialize
+            do
+            {
+                Console.Write("*");
+                i++; // update!
+            } while (i < n); // test condition
+
+        }
+    }
+}
