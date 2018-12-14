195fc7fac6db5c23e8da01ab278f98373903596d
pleasantdevon <44344101+pleasantdevon@users.noreply.github.com>
2018-12-13 22:27:20 -0500

    Add files via upload

---
+++ Lab6/~AutoRecover.Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            
+        }
+           
+        for (int i = 0; i < names.Length; i++)
+			{
+              Console.WriteLine(names[i]);
+			}
+             Console.Readline();
+               
+            
+
+            
+
+
+
+        }
+    }
+}
